46167f9368b07b40243269982ef28ddbc5ff206f
ibss-alt <ibss-alt@specify-portal.calacademy.org>
2021-11-03 14:07:53 -0700

    Updated links for images

--- 2.sh
+++ 2.sh
@@ -8,6 +8,8 @@ fi
 export url="http://localhost:8983/solr/$1/update/csv?commit=true&encapsulator=\"&escape=\&header=true"
 curl $url --data-binary @build/cores/$1/PortalFiles/PortalData.csv -H 'Content-type:application/csv'
 
+find . -name "settings.json" -exec sed -i 's/\"imageBaseUrl\": \"\"/\"imageBaseUrl\": \"http:\/\/10.2.22.30:8080"/g' {} \;
+
 sudo rm -rf /home/specify/webportal/build.old
 sudo mv /home/specify/webportal/build /home/specify/webportal/build.old
 sudo mv build /home/specify/webportal
