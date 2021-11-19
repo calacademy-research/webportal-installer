21f8df398de041a41f776356768f188a02826f11
ibss-alt <ibss-alt@specify-portal.calacademy.org>
2021-11-18 19:03:52 -0800

    Full import

---
+++ import.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+cd $(dirname $0)
+
+all_collections=(botany iz orn orn-en mam)
+
+for t in ${all_collections[@]}; do
+  ./0.sh $t >& 0.$t.log
+done
+
+for t in ${all_collections[@]}; do
+  ./1.sh $t >& 1.$t.log
+done
+
+sleep 120
+
+for t in ${all_collections[@]}; do
+  ./2.sh $t >& 2.$t.log
+done
+
+./3.sh
+
